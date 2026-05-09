ed104b9d9693885497df0f2a8535ddf2de37c335
melounusaty <meloun.usaty@gmail.com>
2026-05-09 09:56:16 +0200

    Update __init__.py

--- custom_components/tp90X_multi/__init__.py
+++ custom_components/tp90X_multi/__init__.py
@@ -5,7 +5,7 @@ from homeassistant.core import HomeAssistant
 
 from .device import TP90xDevice
 
-DOMAIN = "tp90X_multi"
+DOMAIN = "tp90x_multi"
 PLATFORMS = ["sensor", "select", "switch", "number"]
 
 
